ab091231295ccc48b9619229cd118b22e276758a
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-05 13:19:32 +0530

    Update main.py
    Added try catch

--- main.py
+++ main.py
@@ -16,30 +16,37 @@ gender = []
 
 detector = FaceDetector()
 
-for img in os.listdir(path):
-  print(img)
-  if img!='.git':
-    ages = img.split("_")[0]
-    genders = img.split("_")[1]
-    img = cv2.imread(str(path)+"/"+str(img))
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-     
-    # Crop the face out of an image and size the image at 200,200
-    img, bbox = detector.findFaces(img, draw=False) 
-    if bboxs:
-      X, Y, W, H = bbox[0]['bbox']
+try:
+  for img in os.listdir(path):
+    print(img)
+    if img!='.git':
+      ages = img.split("_")[0]
+      genders = img.split("_")[1]
+      img = cv2.imread(str(path)+"/"+str(img))
+      img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+      
+      # Crop the face out of an image and size the image at 200,200
+      img, bbox = detector.findFaces(img, draw=False) 
+      if bbox:
+        X = bbox[0]['bbox'][0]
+        Y = bbox[0]['bbox'][1]
+        W = bbox[0]['bbox'][2]
+        H = bbox[0]['bbox'][3]
 
-      croppedImg = img[Y:Y+H, X:X+W]
-      resizedImg = [cv2.resize(croppedImg, (200, 200))]
-
-      images.append(resizedImg)
-
-      images.append(img)
-      age.append(ages)
-
+        croppedImg = img[Y:Y+H, X:X+W]
+        resizedImg = [cv2.resize(croppedImg, (200, 200))]
+                
+        images.append(resizedImg)
+        
+        images.append(img)
+        age.append(ages)
+except Exception as e:
+  print(e)
 
 age = np.array(age,dtype=np.int64)
 images = np.array(images)
-
-plt.imshow(images[0])
-
+try:
+  print("age:", age)
+  print("Images", images)
+except Exception as e:
+  print(e)
