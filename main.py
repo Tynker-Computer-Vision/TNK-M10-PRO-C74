70403a112b415e59b3f0f9af92bf23d79c3984bd
vishalgddam873 <vishal18@navgurukul.org>
2023-06-05 12:46:51 +0530

    Initial comment

--- main.py
+++ main.py
@@ -26,10 +26,7 @@ for img in os.listdir(path):
      
     # Crop the face out of an image and size the image at 200,200
     img, bbox = detector.findFaces(img, draw=False) 
-    X = bbox[0]['bbox'][0]
-    Y = bbox[0]['bbox'][1]
-    W = bbox[0]['bbox'][2]
-    H = bbox[0]['bbox'][3]
+    X, Y, W, H = bbox[0]['bbox']
 
     croppedImg = img[Y:Y+H, X:X+W]
     resizedImg = [cv2.resize(croppedImg, (200, 200))]
